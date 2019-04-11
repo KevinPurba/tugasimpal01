c27ac8ae989bc5c64656361f5bd58b65ec80763c
KevinPurba <40649256+KevinPurba@users.noreply.github.com>
2019-04-11 15:26:45 +0700

    Add files via upload

---
+++ klinik2/admin/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<center>
+		<h5 style="color:white;"> &copy; <?php echo date("Y"); ?> By Yehezkiel Kevin AP</h5>
+	</center>
+</div>
---
+++ klinik2/admin/includes/header2.php
@@ -0,0 +1,41 @@
+<div class="header">
+	<div class="headerleft">
+		<b>Sistem Managemen Klinik</b>
+	</div>
+	<div class="headerright">
+		<b>
+			<?php
+			require '../includes/connect.php';
+			require '../includes/users.php';
+			admindetails();
+			 ?>
+		</b>
+	</div>
+</div>
+
+<html>
+<head>
+	<title>Sistem Managemen Klinik - Login</title>
+			<html>
+<head><title>Put Image in HTML</title>
+</head>
+
+<center>
+<body background = "">
+
+<img src = "settings.jpg" width="1100px" height="330px"/>
+
+</body></center>
+</html>
+
+<html>
+	<style type="text/css">
+	body
+	{
+
+		background-color: #1E90FF;
+	}
+		
+	</style>
+</head>
+</html>
